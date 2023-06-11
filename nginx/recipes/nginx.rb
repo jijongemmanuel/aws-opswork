121459c04d27f59ae2b05578256d7417a68f8afd
Emmanuel Jijong <98189094+jijongemmanuel@users.noreply.github.com>
2023-06-11 12:27:03 -0400

    Update nginx.rb

--- nginx/recipes/nginx.rb
+++ nginx/recipes/nginx.rb
@@ -2,6 +2,3 @@ package 'nginx'
 service 'nginx' do
 action [:enable, :start]
 end
- file "/var/www/html/index.html" do 
-content "<h1> Hello This is a chef recipe #{node[:name]} and #{node[:age]}</h1>"
-end
